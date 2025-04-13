281bd53989bda385a668e6c46f3a6ed95c6b0d05
Chrb09 <carloshrbarile@gmail.com>
2025-04-13 13:21:27 -0300

    env add

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:3333" });
+export const api = axios.create({ baseURL: import.meta.env.VITE_APP_API_BACKEND ?? "http://localhost:3333" });
